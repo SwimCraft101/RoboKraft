f76325f93edef467424e5872ab51fc77d31a9548
SwimCraft101 <120220459+SwimCraft101@users.noreply.github.com>
2026-06-16 07:12:49 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,43 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Grass":return img`
+7 7 7 7 7 7 7 7 
+e 7 7 7 7 7 7 7 
+e 7 e 7 7 e e e 
+e e e e e e e e 
+e e e e e e e e 
+e e e e e e e e 
+e e e e e e e e 
+e e e e e e e e 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
